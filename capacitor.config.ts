a699a771d3e98f5aa6268fc599ece77d5a51453d
Gonzalo Tevez <gonzalotevez222@gmail.com>
2026-01-26 22:54:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meetly',
+  webDir: 'dist'
+};
+
+export default config;
